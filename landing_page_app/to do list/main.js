3fc695a971a0a591e7fb5319c1cf8412bfc87232
kennyWhiteCreations <52010283+kennyWhiteCreations@users.noreply.github.com>
2020-01-17 18:55:40 -0500

    Add files via upload

--- landing_page_app/to do list/main.js
+++ landing_page_app/to do list/main.js
@@ -1,19 +1,20 @@
-// main dom elements
+
 let input = document.getElementById('input');
 let ul = document.querySelector('ul');
 let addBtn = document.getElementById('add_btn');
-let checkForm = document.getElementById('check-form');
+
+let error = document.getElementById('error-msg');
 
 
 	
 	
 
-// function for add button is clicked
+
 addBtn.onclick = () => {
 	let listItem = input.value;
     input.value = "";
 
-	// created elements of list
+
 	let list = document.createElement('li');
 	let checkBox = document.createElement('input');
 	checkBox.setAttribute('type', 'checkbox');
@@ -26,36 +27,22 @@ addBtn.onclick = () => {
 	dBtn.textContent = "delete";
 	ul.appendChild(list);
 	listText.textContent = listItem;
-	console.log(listItem);
+
+
+
 	dBtn.onclick = () => {
 		ul.removeChild(list);
+	} 
+	if(listItem === "" || listItem == null) {
+		error.textContent = "please don't leave this field empty";
+		input.style.border = 'solid 1px red';
+		ul.removeChild(list);
 	}
-	input.focus();
+	
+		
+	
+
+	
+	
 }
 
-/*checkBox.addEventListener('onclick', () => {
-listText.style.textDecoration = 'line-through';
-})
-*/
- /*checkForm.onsubmit = (e) => {
-	// function that validates input box 
-	// error div
-	let errorMsg = document.getElementById('error-msg'); 
-	input.blur();
-	if (input.value == "" || input.value == null) {
-		errorMsg.textContent = "please enter in this field";
-		input.focus(); 	  
-
-		e.preventDefault();
-
-		return false;
-		
-	} else { 
-		addToList();
-		return true;
-		   }
-		   
-} */
-
-
-//checkForm(e);
